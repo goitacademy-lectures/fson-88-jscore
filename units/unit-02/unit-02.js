25a65070eb887df0b88a2a6074ab487d63ee24ff
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2023-06-23 15:17:01 +0300

    add answers to units 1-3

--- units/unit-02/unit-02.js
+++ units/unit-02/unit-02.js
@@ -1,23 +1,45 @@
 // Task 1.
 // Створіть дві змінні a = 7 та b = 9. Виведіть у консоль результат множення a на b.
+const a = 7;
+const b = 9;
+console.log(a);
+console.log(b);
 
 // Task 2.
 // Створіть дві змінні c = 7 та d = 9. Виведіть на сторінку в .output-2 результат ділення c на d.
+const c = 7;
+const d = 9;
+document.querySelector(".output-2").textContent = c / d;
 
 // Task 3.
 //  Створіть дві змінні e = 3 та f = 5. Виведіть на сторінку в .output-3 результат додавання e + f.
+const e = 7;
+const f = 9;
+document.querySelector(".output-3").textContent = e + f;
 
 // Task 4.
 //  Створіть дві змінні e1 = '3' і f1 = 5. Виведіть на сторінку в .output-4 результат додавання e1 + f1. Поясніть відмінність від попереднього результату.
+const e1 = "3";
+const f1 = 5;
+document.querySelector(".output-4").textContent = e1 + f1;
 
 // Task 5.
 //  Створіть дві змінні e2 = 3 і f2 = 0. Виведіть на сторінку в .output-5 результат ділення e2 на f2.
+const e2 = "3";
+const f2 = 5;
+document.querySelector(".output-5").textContent = e2 / f2;
 
 // Task 6.
 //  Створіть дві змінні e3 = 3 та f3 = 'Hello'. Виведіть на сторінку в .output-6 результат додавання двох змінних.
+const e3 = 3;
+const f3 = "Hello";
+document.querySelector(".output-6").textContent = e3 + f3;
 
 // Task 7.
 //  Створіть дві змінні e4 = 3 та f4 = 'Hello'. Виведіть на сторінку в .output-7 результат множення двох змінних.
+const e4 = 3;
+const f4 = "Hello";
+document.querySelector(".output-7").textContent = e4 * f4;
 
 // Task 8.
 // Після натискання кнопки .btn-8 виконується функція fn8, яка виводить в div.output-8 те, що користувач ввів в input.field-8. Розгляньте код і розберіть роботу скрипта
@@ -34,57 +56,91 @@ document.querySelector(".btn-8").onclick = fn8;
 // Task 9.
 // Після натискання кнопки .btn-9 виконується функція fn9, яка виводить в div.output-9 те, що користувач ввів в input.field-9. Додайте очищення input після натискання кнопки.
 
-function fn9() {}
+function fn9() {
+  const inputEl = document.querySelector(".field-9");
+
+  document.querySelector(".output-9").innerHTML = inputEl.value;
+  inputEl.value = "";
+}
 
 document.querySelector(".btn-9").onclick = fn9;
 
 // Task 10.
 // Після натискання на кнопку .btn-10 виконується функція fn10, яка виводить у div.output-10 те, що користувач ввів в input.field-10 помножене на 20.
 
-function fn10() {}
+function fn10() {
+  const inputEl = document.querySelector(".field-10");
+  document.querySelector(".output-10").innerHTML = inputEl.value * 20;
+}
 
 document.querySelector(".btn-10").onclick = fn10;
 
 // Task 11.
 // Після натискання на кнопку .btn-11 виконується функція fn11, яка виводить на сторінку в .output-11 число, яке ввів користувач в .field-11 і до нього додане число 55. У цій задачі ми не приводимо до числа значення отримане з input.
 
-function fn11() {}
+function fn11() {
+  const inputEl = document.querySelector(".field-11");
+  document.querySelector(".output-11").innerHTML = inputEl.value + 55;
+}
 
 document.querySelector(".btn-11").onclick = fn11;
 
 // Task 12.
 // Після натискання на кнопку виконується функція fn12. Функція повинна отримати дані з input.field-12-1 та .field-12-2 (користувач вводить в них прізвище та ім'я). Після чого вивести в .output-12 рядок 'Hello ім'я прізвище', де ім'я - це ім'я користувача, а прізвище - це введене прізвище.
-// Звертаємо увагу на те, що ми працюємо у відповідності до завданням.Hello - з великої літери. Між Hello ім'я один пробіл
+// Звертаємо увагу на те, що ми працюємо у відповідності до завдання.Hello - з великої літери. Між Hello ім'я один пробіл
 
-function fn12() {}
+function fn12() {
+  const userName = document.querySelector(".field-12-1").value;
+  const userSurname = document.querySelector(".field-12-2").value;
+
+  console.log(userName);
+  console.log(userSurname);
+
+  document.querySelector(".output-12").innerHTML = `Hello ${userName} ${userSurname}`;
+}
 
 document.querySelector(".btn-12").onclick = fn12;
 
 // Task 13.
 // При натисканні кнопки .btn-13 виконується функція fn13. Функція отримує з input.field-13-1 та input.field-13-2 числа в змінні a та b. Після чого в .output-13 виводить їхню суму. Потрібно перетворити змінні a та b у числа!
 
-function fn13() {}
+function fn13() {
+  const firstNumber = document.querySelector(".field-13-1").value;
+  const secondNumber = document.querySelector(".field-13-2").value;
+
+  document.querySelector(".output-13").innerHTML = Number(firstNumber) + Number(secondNumber);
+}
 
 document.querySelector(".btn-13").onclick = fn13;
 
 // Task 14.
 // Після натискання кнопки .btn-14 виконується функція fn14. Вона замінює значення value input.field-14 на рядок 'Go'
 
-function fn14() {}
+function fn14() {
+  document.querySelector(".field-14").value = "Go";
+}
 
 document.querySelector(".btn-14").onclick = fn14;
 
 // Task 15.
 //  Після натискання кнопки .btn-15 виконується функція fn15. Вона встановлює для input.field-15 властивість style.border = "4px solid red".
 
-function fn15() {}
+function fn15() {
+  const inputEl = document.querySelector(".field-15");
+  inputEl.style.border = "4px solid red";
+}
 
 document.querySelector(".btn-15").onclick = fn15;
 
 // Task 16.
 // Після натискання кнопки .btn-16 виконується функція fn16. Функція отримує з .field-16-1 та field-16-2 числа. Виведіть у .output-16 суму даних чисел по натисканню кнопки .btn-16. Не переводимо отримані значення з input у число!
 
-function fn16() {}
+function fn16() {
+  const firstValue = document.querySelector(".field-16-1").value;
+  const secondValue = document.querySelector(".field-16-2").value;
+
+  document.querySelector(".output-16").innerHTML = firstValue + secondValue;
+}
 
 document.querySelector(".btn-16").onclick = fn16;
 
@@ -93,7 +149,7 @@ document.querySelector(".btn-16").onclick = fn16;
 
 function fn17() {
   let a = document.querySelector(".field-17").value;
-  document.querySelector(".output-17-1").innerHTML = +a;
+  document.querySelector(".output-17-1").innerHTML = +a; // оператор + перед змінною спробує привести заначення до числа
   document.querySelector(".output-17-2").innerHTML = parseInt(a, 10);
   document.querySelector(".output-17-3").innerHTML = Number(a);
 }
@@ -103,14 +159,22 @@ document.querySelector(".btn-17").onclick = fn17;
 // Task 18.
 // Після натискання кнопки .btn-18 виконується функція fn18. Функція отримує з .field-18 число, яку потірбно присвоїти у змінну a. Виводить в output-18 результат операції parseFloat(a). Спробуйте ввести значення 5, 33.02, 33.9a, -20.01
 
-function fn18() {}
+function fn18() {
+  const a = document.querySelector(".field-18").value;
+  document.querySelector(".output-18").innerHTML = parseFloat(a);
+}
 
 document.querySelector(".btn-18").onclick = fn18;
 
 // Task 19.
 // Після натискання кнопки .btn-19 функція fn19 повинна в .output-19 вивести суму двох відʼємних чисел із input.field-19-1 та input.field-19-2.
 
-function fn19() {}
+function fn19() {
+  const firstNumber = document.querySelector(".field-19-1").value;
+  const secondNumber = document.querySelector(".field-19-2").value;
+
+  document.querySelector(".output-19").innerHTML = Number(firstNumber) + Number(secondNumber);
+}
 
 document.querySelector(".btn-19").onclick = fn19;
 
@@ -121,7 +185,9 @@ let count = 0;
 
 function fn20() {
   // порахувати нове значення для count
+  count += 1;
   // нижче додати вивід в .output-20
+  document.querySelector(".output-20").innerHTML = count;
 }
 
 document.querySelector(".btn-20").onclick = fn20;
--- units/unit-03/unit-03.js
+++ units/unit-03/unit-03.js
@@ -1,17 +1,34 @@
 // Task 1
 // При натисканні кнопки .btn-1 спрацьовує функція fn1. Функція повинна прочитати вміст .field-1 та порівняти його з числом 4 (порівняння ==). Результат порівняння – true або false виведіть у .output-1.
 
-function fn1() {}
+function fn1() {
+  const inputData = document.querySelector(".field-1").value;
+  const output = document.querySelector(".output-1");
+
+  if (inputData == 4) {
+    output.innerHTML = true;
+    return;
+  }
+
+  output.innerHTML = false;
+}
 
 document.querySelector(".btn-1").onclick = fn1;
 
 // Task 2
 // Дано дві змінні a2_1 і a2_2. При натисканні кнопки .btn-2 запускається функція fn2. Функція повинна порівняти змінні за допомогою if else і вивести в .output-2 більше число. Варіант рівності змінних не розглядається.
+let a2_1 = 45;
+let a2_2 = 32;
 
-// let a2_1 = ;
-// let a2_2 = ;
+function fn2() {
+  const output = document.querySelector(".output-2");
 
-function fn2() {}
+  if (a2_1 > a2_2) {
+    output.innerHTML = a2_1;
+  } else {
+    output.innerHTML = a2_2;
+  }
+}
 
 document.querySelector(".btn-2").onclick = fn2;
 
@@ -19,42 +36,109 @@ document.querySelector(".btn-2").onclick = fn2;
 // Дано два input - .field-31 і .field-32, обидва - input[type=number]. При натисканні кнопки .btn-3 спрацьовує функція fn3. Функція має порівняти числа з input, вивести в .output-3 більше число.
 // Проведіть самостійний тест роботи, введіть пари чисел 4 та 9, 9 та 22, 5 та 111.
 
-function fn3() {}
+function fn3() {
+  const firstInputValue = Number(document.querySelector(".field-31").value);
+  const secondInputValue = Number(document.querySelector(".field-32").value);
+  const output = document.querySelector(".output-3");
+
+  if (firstInputValue > secondInputValue) {
+    output.innerHTML = firstInputValue;
+    return;
+  }
+
+  output.innerHTML = secondInputValue;
+}
 
 document.querySelector(".btn-3").onclick = fn3;
 
 // Task 4.
 // Користувач вводить у .field-4 рік свого народження. Є кнопка .btn-4, яка запускає функцію fn4.Функція повинна вивести в .output-4 число 1 якщо користувачеві більше або 18 років, і 0 якщо менше.
 
-function fn4() {}
+function fn4() {
+  // const currentDate = new Date();
+  // currentDate.getFullYear() // отримання поточної дати через js
+
+  const currentDate = 2023;
+  const userBirthDay = Number(document.querySelector(".field-4").value);
+  const output = document.querySelector(".output-4");
+
+  if (currentDate - userBirthDay >= 18) {
+    output.innerHTML = 1;
+    return;
+  }
+
+  output.innerHTML = 0;
+}
 
 document.querySelector(".btn-4").onclick = fn4;
 
 // Task 5.
 // На сторінці є input з класом .field-5, куди можна ввести число. Є кнопка .btn-5, яка запускає функцію fn5. Функція повинна вивести в .output-5 символ 'm' якщо число менше нуля, 0 якщо число дорівнює нулю і 1 якщо число більше нуля.
 
-function fn5() {}
+function fn5() {
+  const inputValue = Number(document.querySelector(".field-5").value);
+  const output = document.querySelector(".output-5");
+
+  if (inputValue < 0) {
+    output.innerHTML = "m";
+  } else if (inputValue === 0) {
+    output.innerHTML = 0;
+  } else {
+    output.innerHTML = 1;
+  }
+}
 
 document.querySelector(".btn-5").onclick = fn5;
 
 // Task 6.
 // На сторінці є input.field-6, куди користувач може ввести число. Є кнопка .btn-6, яка запускає функцію fn6. Функція повинна вивести в .output-6 слово 'even' якщо число парне і 'odd' якщо непарне. Для перевірки парності використовується залишок від ділення на 2 (оператор %). Якщо залишок дорівнює нулю – парне, ні – непарне.
 
-function fn6() {}
+function fn6() {
+  const inputValue = Number(document.querySelector(".field-6").value);
+  const output = document.querySelector(".output-6");
+
+  if (inputValue % 2 === 0) {
+    output.innerHTML = "even";
+  } else {
+    output.innerHTML = "odd";
+  }
+}
 
 document.querySelector(".btn-6").onclick = fn6;
 
 // Task 7.
 // Дано два input - .field-7-1 і .field-7-2, обидва - input[type=number]. При натисканні кнопки .btn-7 спрацьовує функція fn7. Функція повинна число з .field-7-1 піднести до ступеня .field-7-2, вивести результат у .output-7. Для піднесення до ступеня можна використовувати ** або Math.pow.
 
-function fn7() {}
+function fn7() {
+  const exponentBase = Number(document.querySelector(".field-7-1").value);
+  const exponent = Number(document.querySelector(".field-7-2").value);
+  const output = document.querySelector(".output-7");
+
+  output.innerHTML = Math.pow(exponentBase, exponent);
+}
 
 document.querySelector(".btn-7").onclick = fn7;
 
 // Task 8.
 // Дано select.select-8, який містить 3 значення: 1, 2, 3. Дана кнопка btn-8. При натисканні спрацьовує функція fn8. Функція повинна отримати обране в select число, потім за допомогою switch case порівняти його по черзі з 1, 2, 3. І якщо вибране число - 1, то вивести в .output-8 рядок 'one', якщо 2 - 'two', якщо 3 - 'three'.
 
-function fn8() {}
+function fn8() {
+  const selectValue = Number(document.querySelector(".select-8").value);
+  const output = document.querySelector(".output-8");
+
+  switch (selectValue) {
+    case 1:
+      output.innerHTML = "one";
+      break;
+    case 2:
+      output.innerHTML = "two";
+      break;
+
+    default:
+      output.innerHTML = "three";
+      break;
+  }
+}
 
 document.querySelector(".btn-8").onclick = fn8;
 
@@ -65,48 +149,123 @@ document.querySelector(".btn-8").onclick = fn8;
 //  якщо від 44 (включно) до 64 (включно) – то 3
 //  інакше вивести 0
 
-function fn9() {}
+function fn9() {
+  const inputValue = Number(document.querySelector(".field-9").value);
+  const output = document.querySelector(".output-9");
+
+  if (inputValue >= 1 && inputValue <= 32) {
+    output.innerHTML = 1;
+  } else if (inputValue >= 33 && inputValue <= 43) {
+    output.innerHTML = 2;
+  } else if (inputValue >= 44 && inputValue <= 64) {
+    output.innerHTML = 3;
+  } else {
+    output.innerHTML = 0;
+  }
+}
 
 document.querySelector(".btn-9").onclick = fn9;
 
 // Task 10
 // Дано select.select-10. Після натискання кнопки, виведіть value вибраного option в .output-10.
 
-function f10() {}
+function f10() {
+  const selectValue = document.querySelector(".select-10").value;
+  const output = document.querySelector(".output-10");
+
+  output.innerHTML = selectValue;
+}
 
 document.querySelector(".btn-10").onclick = f10;
 
 // Task 11
 // Дано select.select-11. Після зміни стану select (подія onchange) виведіть value вибраного option в .output-11.
 
-function f11() {}
+function f11() {
+  const selectValue = document.querySelector(".select-11").value;
+  const output = document.querySelector(".output-11");
 
-document.querySelector(".select-110").onchange = f11;
+  output.innerHTML = selectValue;
+}
+
+document.querySelector(".select-11").onchange = f11;
 
 // Task 12
 //  Дано input.field-12. Після натискання кнопки отримайте значення з input в змінну, а потім виведіть в .output-12 typeof отриманої змінної. typeof дозволяє визначити тип даних.
 
-function f12() {}
+function f12() {
+  const inputValue = document.querySelector(".field-12").value;
+  const output = document.querySelector(".output-12");
+
+  output.innerHTML = typeof inputValue;
+}
 
 document.querySelector(".btn-12").onclick = f12;
 
 // Task 13
 //  Дано input.field-13. На відміну від попереднього завдання – input type number. Після натискання кнопки отримайте значення з input в змінну, а потім виведіть в out-13 typeof отриманої змінної. Typeof дозволяє визначити тип даних. Якщо ви правильно все зробили - тип даних буде string! Подумайте, чому так?
 
-function f13() {}
+function f13() {
+  const inputValue = document.querySelector(".field-13").value;
+  const output = document.querySelector(".output-13");
+
+  output.innerHTML = typeof inputValue;
+}
 
 document.querySelector(".btn-13").onclick = f13;
 
 // Task 14
 // Дано input .field-14-1 та .field-14-2, [type=number]. Дано select .select-143, який містить чотири операції - +, -, *, /. Дано кнопку .btn-14, при натисканні на яку спрацьовує функція fn14. Функція виводить в .output-14 результат операції обраної в 3-му select до числа введених у першому і другому input. Наприклад, вибрано 1 13 +, потрібно вивести результат операції 1+13 тобто.
 
-function f14() {}
+function f14() {
+  const firstInputValue = Number(document.querySelector(".field-14-1").value);
+  const secondInputValue = Number(document.querySelector(".field-14-2").value);
+  const selectValue = document.querySelector(".select-143").value;
+  const output = document.querySelector(".output-14");
+
+  let result = null;
+
+  switch (selectValue) {
+    case "+":
+      result = firstInputValue + secondInputValue;
+      break;
+    case "-":
+      result = firstInputValue - secondInputValue;
+      break;
+    case "*":
+      result = firstInputValue * secondInputValue;
+      break;
+    case "/":
+      result = firstInputValue / secondInputValue;
+      break;
+  }
+
+  output.innerHTML = result;
+}
 
 document.querySelector(".btn-14").onclick = f14;
 
 // Task     15
 // Дано select .select-15-1 та .select-15-2, кожен з яких містить 1 та 0. Дано select .select-15-3, який містить дві операції - && та || . Дана кнопка .btn-15, при натисканні на яку спрацьовує функція fn15. Функція виводить в .output-15 результат логічних операцій вибраних у третьому select до числа вибраних у першому і другому select. Наприклад, вибрано '1 1 &&', потрібно вивести результат операції '1 && 1', тобто 1 чи 0.
 
-function f15() {}
+function f15() {
+  const firstSelectValue = Number(document.querySelector(".select-15-1").value);
+  const secondSelectValue = Number(document.querySelector(".select-15-2").value);
+  const operand = document.querySelector(".select-15-3").value;
+  const output = document.querySelector(".output-15");
+
+  let result = null;
+
+  switch (operand) {
+    case "&&":
+      result = firstSelectValue && secondSelectValue;
+      break;
+    case "||":
+      result = firstSelectValue || secondSelectValue;
+      break;
+  }
+
+  output.innerHTML = result;
+}
 
 document.querySelector(".btn-15").onclick = f15;
