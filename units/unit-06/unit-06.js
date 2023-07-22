39deca0b136287785cd20762f5dcb3cbdca779b0
Olegmkv <129410354+Olegmkv@users.noreply.github.com>
2023-07-22 15:49:36 +0300

    unit 6

--- units/unit-06/unit-06.js
+++ units/unit-06/unit-06.js
@@ -47,27 +47,34 @@ document.querySelector(".btn-3-2").onclick = function () {
 // Task 4
 // Напишіть функцію fn4, яка приймає ваш рік народження і обчислює (повертає) ваш вік. Використайте конструктор new Date() і його метод getFullYear()
 
-function fn4() {
-  
+function fn4(youYear) {
+  const dataToday = new Date();
+  const yearToday = dataToday.getFullYear(); 
+
+  return yearToday - youYear;
 }
 
 document.querySelector(".btn-4").onclick = function () {
-  document.querySelector(".output-4").textContent = fn4(2000); // тут можна вказати свій вік
+  document.querySelector(".output-4").textContent = fn4(1970); // тут можна вказати свій вік
 };
 
 // Task 5
 // Напишіть функцію fn5, яка приймає ваше ім'я як параметр і повертає число символів у ньому, де name - це ім'я прийняте як параметр. Довжину (число символів) можна порахувати за допомогою властивості length.
 
-function fn5() {}
+function fn5(youName) {
+  return `${youName} ${youName.length}`
+}
 
 document.querySelector(".btn-5").onclick = function () {
-  document.querySelector(".output-5").textContent = fn5("Mango"); // тут можна вказати своє ім'я
+  document.querySelector(".output-5").textContent = fn5("Oleg"); // тут можна вказати своє ім'я
 };
 
 // Task 6
 // Напишіть функцію fn6, яка приймає 2 числа і повертає випадкове ціле число від першого до другого прийнятого параметра. Це перше завдання, вирішення якого потрібно знайти в Google.
 
-function fn6() {}
+function fn6(arg1, arg2) {
+  return Math.round(Math.random() * (arg1 - arg2) + arg2);
+}
 
 document.querySelector(".btn-6").onclick = function () {
   document.querySelector(".output-6").textContent = fn6(100, 107); // виклик функції
@@ -81,6 +88,10 @@ function fn7() {
   // Нам потрібно зробити такий рядок, наприклад:
   // rgb(33, 123, 255) - числа можуть бути будь-які від 0 до 255
   // У вас є функція fn6, яка може генерувати випадкові цілі числа в потрібному діапазоні, давайте скористаємося нею
+  const redColor = Math.round(Math.random() * 255);
+  const greenColor = Math.round(Math.random() * 255);
+  const blueColor = Math.round(Math.random() * 255);
+  return `rgb(${redColor}, ${greenColor}, ${blueColor})`
 }
 
 document.querySelector(".btn-7").onclick = function () {
@@ -90,7 +101,9 @@ document.querySelector(".btn-7").onclick = function () {
 // Task 8
 // Напишіть функцію fn8, яка приймає рядок як параметр і повертає результат з очищеними пробілами на початку та в кінці рядка. Рядок, який приймає функція "    JavaScriptIsMyJam    ", а повертає санітізований рядок JavaScriptIsMyJam. Для видалення пробілів використайте метод trim().
 
-function fn8() {}
+function fn8(inputString) {
+  return inputString.trim();
+}
 
 document.querySelector(".btn-8").onclick = function () {
   let s = "            JavaScriptIsMyJam            ";
@@ -101,17 +114,21 @@ document.querySelector(".btn-8").onclick = function () {
 // Task 9
 //  Напишіть функцію fn9, яка приймає число та повертає true, якщо число парне, і false якщо не парне.
 
-function fn9() {}
+function fn9(Arg) {
+  return Arg % 2 === 0;
+}
 
 document.querySelector(".btn-9").onclick = function () {
-  document.querySelector(".output-9").textContent = fn9(15); // виклик функції
+  document.querySelector(".output-9").textContent = fn9(14); // виклик функції
 };
 
 // Task 10
 // Створіть функцію fn10, яка приймає 2 числа і повертає більше з них. У разі рівності - перше.
 
-function fn10() {}
+function fn10(arg1, arg2) {
+  return arg1 >= arg2 ? arg1 : arg2;
+}
 
 document.querySelector(".btn-10").onclick = function () {
-  document.querySelector(".output-10").textContent = fn10(3, 9); // виклик функції
+  document.querySelector(".output-10").textContent = fn10(9, 9); // виклик функції
 };
